eeb816ac33c4ed8c6ab380f1acfb45bbbf7cf36b
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2025-09-22 17:54:22 -0400

    Edit responsive sizing for hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,10 +3,10 @@ import LinkButton from './LinkButton';
 const Hero = () => {
 	return (
 		<section className="mt-14 flex w-full flex-col items-center gap-2 text-center md:mt-16 md:gap-4">
-			<h1 className="w-xs md:w-md text-4xl font-bold md:text-6xl">
+			<h1 className="w-full max-w-sm  md:max-w-md text-5xl font-bold md:text-6xl">
 				From Paycheck to Portfolio
 			</h1>
-			<p className="w-2xs text-sm md:w-sm md:text-lg">
+			<p className="w-full max-w-xs text-base md:max-w-sm md:text-lg">
 				Turn your income into investments with simple, automated
 				allocation tools.
 			</p>
